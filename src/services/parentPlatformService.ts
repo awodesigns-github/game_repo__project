f39ca0c3d31233cd8044c89d2ac70dceccd7af06
joseph <awo.designs1@gmail.com>
2025-06-20 14:16:15 +0300

    create parentPlatform service

---
+++ src/services/parentPlatformService.ts
@@ -0,0 +1,19 @@
+import createInstance from "@/services/httpService.ts";
+
+interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+    games_count: number;
+    year_start: string;
+    year_end: string;
+}
+
+export interface ParentPlatform {
+    id: number;
+    name: string;
+    slug: string;
+    platforms: Platform[];
+}
+
+export default createInstance('/platforms/lists/parents');
