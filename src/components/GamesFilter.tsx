51452c2a9cdf7a3cf35014779372627299789e5a
joseph <awo.designs1@gmail.com>
2025-06-20 14:50:12 +0300

    Implement select element for platform filter

--- src/components/GamesFilter.tsx
+++ src/components/GamesFilter.tsx
@@ -1,53 +1,58 @@
-import {Box, createListCollection, Heading, Portal, Select, SelectItemIndicator} from "@chakra-ui/react";
+import {Box, Heading, NativeSelect} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import parentPlatformService, {type ParentPlatform} from "@/services/parentPlatformService.ts";
+import {type ApiResponse, CanceledError} from "@/services/apiClient.ts";
 
 interface GamesFilterProps {
     title: string;
 }
 
-const testList = createListCollection({
-    items: [
-        { label: 'Xbox', value: 'xbox' },
-        { label: 'Playstation', value: 'play-station' },
-        { label: 'PC', value: 'pc' },
-        { label: 'MacOs', value: 'mac-os' },
-    ]
-})
-
 const GamesFilter = ({title}: GamesFilterProps) => {
+    const [platformsList, setPlatformsList] = useState<ParentPlatform[]>([]);
+    const [error, setError] = useState('');
+    const [selectValue, setSelectValue] = useState<string>('');
+
+    useEffect(() => {
+       const { request, cancel } = parentPlatformService.getAll<ApiResponse<ParentPlatform>>();
+
+       request
+           .then(res => setPlatformsList(res.data.results))
+           .catch(error => {
+               if (error instanceof CanceledError)
+                   return;
+               setError(error.message)
+           })
+
+       return (() => cancel())
+    }, []);
+
+
     return (
         <>
             <Heading size={'5xl'}>{title} Games</Heading>
             <Box my={5}>
-                <Select.Root
-                    collection={testList}
-                    size={'sm'}
-                    width={'320px'}
+                <NativeSelect.Root
+                    width={'sm'}
+                    variant={'subtle'}
                 >
-                    <Select.HiddenSelect />
-                    <Select.Control>
-                        <Select.Trigger>
-                            <Select.ValueText placeholder={'All Categories'} />
-                        </Select.Trigger>
-                        <Select.IndicatorGroup>
-                            <Select.ClearTrigger />
-                            <Select.Indicator />
-                        </Select.IndicatorGroup>
-                    </Select.Control>
-                    <Portal>
-                        <Select.Positioner>
-                            <Select.Content>
-                                {
-                                    testList.items.map((test) => (
-                                        <Select.Item key={test.label} item={test.value}>
-                                            {test.label}
-                                            <SelectItemIndicator />
-                                        </Select.Item>
-                                    ))
-                                }
-                            </Select.Content>
-                        </Select.Positioner>
-                    </Portal>
-                </Select.Root>
+                    <NativeSelect.Field
+                        value={selectValue}
+                        onChange={(e) => setSelectValue(e.target.value)}
+                    >
+                        <option value={''}>All categories</option>
+                        {
+                            platformsList.map(platform => (
+                                <option
+                                    key={platform.id}
+                                    value={platform.slug}
+                                >
+                                    {platform.name}
+                                </option>
+                            ))
+                        }
+                    </NativeSelect.Field>
+                    <NativeSelect.Indicator />
+                </NativeSelect.Root>
             </Box>
         </>
     )
